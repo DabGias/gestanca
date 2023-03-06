ea65aedc1004890073e272befe2a7f987037a7bb
DabGias <gabrielfurlaneti@gmail.com>
2023-03-06 11:55:27 -0300

    criação de models e controllers

---
+++ src/main/java/br/com/fiap/gestanca/controllers/DespesaController.java
@@ -0,0 +1,17 @@
+package br.com.fiap.gestanca.controllers;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.models.Despesa;
+
+@RestController
+public class DespesaController {
+    @GetMapping("/api/despesas")
+    public Despesa index() {
+        return new Despesa(new BigDecimal(100), LocalDate.now(), "Cineminha");
+    }
+}
